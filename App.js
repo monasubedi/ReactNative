6f0488e037cb10ad7d66ccf523a8ae8800577e09
monasubedi <sbd.mona@gmail.com>
2024-03-07 17:38:46 -0600

    add feature one

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Greeting from './Greeting';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Welcome to CI/CD example</Text>
+      <Greeting />
       <StatusBar style="auto" />
     </View>
   );
---
+++ Greeting.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, TextInput, View } from 'react-native'
+
+const Greeting = () => {
+    return (
+        <View>
+            <TextInput value='Mona Subedi' />
+            <Text>Welcome to CI/CD example</Text>
+        </View>
+
+    )
+}
+
+export default Greeting
